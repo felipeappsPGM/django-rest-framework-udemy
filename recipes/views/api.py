b4a3c2c6660cf726f28c4dc29763fd37847e36b9
felipe.chagas <felypegt8@gmail.com>
2024-05-19 08:27:22 -0300

    295. Completando o CRUD com Get, Post, Patch e Delete no DRF

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -23,26 +23,54 @@ def recipe_api_list(request):
     elif request.method == 'POST':
         print(request.data)
         serializer = RecipeSerializer(
-            data=request.data,
+             data=request.data, 
+            context={'request': request},
+           
         )
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        serializer.save(
+            
+        )
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
             
 
 
-@api_view(http_method_names=['GET', 'POST', 'OPTIONS'])
+@api_view(http_method_names=['GET', 'PATCH', 'OPTIONS', 'DELETE'])
 def recipe_api_detail(request, pk):
     recipe = get_object_or_404(
          Recipe.objects.get_published(), pk=pk
      )
-    serializer = RecipeSerializer(
-        instance=recipe,
-        many=False,
-        context={'request': request}
-    )
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            many=False,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+    
+    elif request.method == 'PATCH':
+        ...
+        serializer = RecipeSerializer(
+            instance=recipe, #enviar dados
+            data=request.data,  #receber dados
+            many=False,
+            context={'request': request},
+            partial= True, # indicar que quer editar apenas um campo ou mais
+        )
+        
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            
+        )
+        return Response(
+            serializer.data,
+            )
+        
+        
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     #recipe = Recipe.objects.get_published().filter(pk=pk).first()
     
