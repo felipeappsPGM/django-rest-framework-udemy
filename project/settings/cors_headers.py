7c7da689390e635a366cbac2a68a045c52b5dac1
felipe.chagas <felypegt8@gmail.com>
2024-05-23 20:05:21 -0300

    add variaveis no env

--- project/settings/cors_headers.py
+++ project/settings/cors_headers.py
@@ -1,3 +1,5 @@
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5000'
-]
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
+CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
